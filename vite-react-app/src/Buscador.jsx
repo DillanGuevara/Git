ca7557475c18886ded6601041ea725a50dbdf5e2
DillanGuevara <131322094+DillanGuevara@users.noreply.github.com>
2024-12-02 13:28:08 -0500

    Delete vite-react-app directory

--- vite-react-app/src/Buscador.jsx
+++
@@ -1,7 +0,0 @@
-export const Buscador = ({ onSubmit, valorInput, onChange }) => {
-    return (
-        <form className="form" onSubmit={onSubmit}>
-            <input className="buscador" value={valorInput} onChange={onChange} placeholder="Busca tus gifs favoritos aquí..." />
-        </form>
-    )
-}
